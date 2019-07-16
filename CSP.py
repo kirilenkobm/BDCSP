c88a67fa865eb215171aafe317788b5e7f4bd700
Bogdan Kirilenko <kirilenkobm@gmail.com>
2019-07-16 11:51:57 +0200

    Wrapped CSP solver in a class

--- CSP.py
+++ CSP.py
@@ -16,7 +16,147 @@ __version__ = "0.03"
 __email__ = "kirilenkobm@gmail.com"
 __status__ = "Development"
 
-CHARS = {"1", "0"}
+
+class BDCspSolver:
+    """Interface between python and C."""
+    def __init__(self, input_strings, k):
+        """Check lib and read input."""
+        self.input_strings = input_strings
+        self.str_num = len(input_strings)
+        self.str_len = len(input_strings[0])
+        self.k = k
+        self.__check_lib()
+        self.__extract_patterns()
+        self.__reformate_pattern_id_to_pos()
+        pass
+
+    @staticmethod
+    def flatten(lst):
+        """Flatten a list of lists into a list."""
+        return [item for sublist in lst for item in sublist]
+
+    @staticmethod
+    def invert_pattern(pattern):
+        """Return NOT pattern."""
+        return [0 if v == 1 else 1 for v in pattern]
+
+    def __check_lib(self):
+        """Check shared lib, compile if needed."""
+        LIBS_DIR = os.path.join(os.getcwd(), "libs")
+        CSP_LIB_PATH = os.path.join(LIBS_DIR, "CSP.so")
+        if not os.path.isfile(CSP_LIB_PATH):
+            eprint("Error: shared libs not found\nCalling make...")
+            os.mkdir("libs") if not os.path.isdir("libs") else None
+            rc = subprocess.call("make", shell=True)
+            sys.exit("Make failed") if rc != 0 else eprint("Make successful")
+        self.CSP_SLIB = ctypes.CDLL(CSP_LIB_PATH)
+        # arguments:
+        self.CSP_SLIB.solve_CSP.argtypes = [ctypes.c_uint32,  # int 5 times
+                                            ctypes.c_uint32,
+                                            ctypes.c_uint32,
+                                            ctypes.c_uint32,
+                                            ctypes.c_uint32,  # int* 6 times
+                                            ctypes.POINTER(ctypes.c_uint8),
+                                            ctypes.POINTER(ctypes.c_uint32),
+                                            ctypes.POINTER(ctypes.c_uint32),
+                                            ctypes.POINTER(ctypes.c_uint32),
+                                            ctypes.POINTER(ctypes.c_uint32),
+                                            ctypes.POINTER(ctypes.c_uint32)]
+        # returns bool
+        self.CSP_SLIB.solve_CSP.restype = ctypes.c_bool
+
+    def __extract_patterns(self):
+        """Extract patterns from the strings."""
+        columns = [[s[n] for s in self.input_strings] for n in range(self.str_len)]
+        self.actual_col_num = self.str_len
+        self.id_to_pattern = []
+        self.pattern_id_to_positions_raw = {}
+        self.pattern_to_positions = defaultdict(list)
+        for num, column in enumerate(columns):
+            if len(set(column)) == 1:
+                self.actual_col_num -= 1
+                continue
+            ones_pattern = tuple(column)
+            zeros_pattern = tuple(self.invert_pattern(ones_pattern))
+            # two patterns per position
+            self.pattern_to_positions[ones_pattern].append(num)
+            self.pattern_to_positions[zeros_pattern].append(num)
+        patterns_sorted = sorted([p for p in self.pattern_to_positions.keys()],
+                                key=lambda x: (sum(x), -1 * x.index(1)),
+                                reverse=True)
+        for num, pattern in enumerate(patterns_sorted):
+            self.id_to_pattern.append(pattern)
+            self.pattern_id_to_positions_raw[num] = self.pattern_to_positions[pattern]
+        sys.exit("Answer is:\nTrue") if self.actual_col_num == 0 else None
+        sys.exit("Answer is:\nTrue") if (self.actual_col_num - self.k) <= 0 else None
+    
+    def __reformate_pattern_id_to_pos(self):
+        """Make a list instead of dict."""
+        max_key = max(self.pattern_id_to_positions_raw.keys())
+        self.pattern_id_to_positions = [[] for _ in range(max_key + 1)]
+        positions_to_patterns_ = defaultdict(list)
+        for pat_id in range(max_key + 1):
+            pos_ = self.pattern_id_to_positions_raw[pat_id]
+            self.pattern_id_to_positions[pat_id] = pos_
+            for p_ in pos_:
+                positions_to_patterns_[p_].append(pat_id)
+        self.all_positions = sorted(positions_to_patterns_.keys())
+        self.pos_to_pat_ids = [tuple() for _ in range(max(self.all_positions) + 1)]
+        for pos_ in self.all_positions:
+            pat_ids = tuple(sorted(positions_to_patterns_[pos_]))
+            self.pos_to_pat_ids[pos_] = pat_ids
+        self.patterns_num = len(self.id_to_pattern)
+        self.positions_num = len(self.all_positions)
+
+    def get_answer(self):
+        """Solve CSP."""
+        # pre-process input \put this stuff into C function
+        # pattern lenght is the str_num
+        patterns_array = self.flatten(self.id_to_pattern)
+        c_patterns_array = (ctypes.c_uint8 * (self.patterns_num * self.str_num + 1))()
+        c_patterns_array[:-1] = patterns_array
+        # patterns to positions
+        pat_to_pos_array = self.flatten(self.pattern_id_to_positions)
+        pat_to_pos_num = [len(x) for x in self.pattern_id_to_positions]
+        pat_to_pos_arr_size = len(pat_to_pos_array)
+        c_pat_to_pos_arr = (ctypes.c_uint32 * (pat_to_pos_arr_size + 1))()
+        c_pat_to_pos_arr[:-1] = pat_to_pos_array
+        c_pat_to_pos_num = (ctypes.c_uint32 * (self.patterns_num + 1))()
+        c_pat_to_pos_num[:-1] = pat_to_pos_num
+        pointer = 0
+        pat_to_pos_starts = []
+        for pos_ in self.pattern_id_to_positions:
+            pat_to_pos_starts.append(pointer)
+            pointer += len(pos_)
+        c_pat_to_pos_starts = (ctypes.c_uint32 * (self.patterns_num + 1))()
+        c_pat_to_pos_starts[:-1] = pat_to_pos_starts
+        # positions to patterns
+        pos_to_patterns_array = self.flatten(self.pos_to_pat_ids)
+        positions_num = len(self.all_positions)
+        c_pos_to_pat_array = (ctypes.c_uint32 * (self.positions_num * 2 + 1))()
+        c_pos_to_pat_array[:-1] = pos_to_patterns_array
+        # positions themselves
+        c_pos_array = (ctypes.c_uint32 * (positions_num + 1))()
+        c_pos_array[:-1] = self.all_positions
+        # single values
+        c_str_num = ctypes.c_uint32(self.str_num)
+        c_str_len = ctypes.c_uint32(self.actual_col_num)
+        c_k_ = ctypes.c_uint32(self.k)
+        c_pos_num = ctypes.c_uint32(self.positions_num)
+        c_pat_num = ctypes.c_uint32(self.patterns_num)
+        # call the solver finally
+        answer = self.CSP_SLIB.solve_CSP(c_str_num,  # int
+                                         c_str_len,  # int
+                                         c_k_,  # int
+                                         c_pat_num,  # int
+                                         c_pos_num,  # int
+                                         c_patterns_array,  # int*
+                                         c_pat_to_pos_arr,  # int*
+                                         c_pat_to_pos_num,  # int*
+                                         c_pat_to_pos_starts,  # int*
+                                         c_pos_array,
+                                         c_pos_to_pat_array)  # int*
+        return answer
 
 
 def parse_args():
@@ -24,7 +164,7 @@ def parse_args():
     app = argparse.ArgumentParser()
     app.add_argument("input_file", help="File containing strings.")
     app.add_argument("k", type=int, help="K to test.")
-    app.add_argument("--verbose", "-v", action="store_true", dest="verbose")
+    # TODO: verbosity more
     app.add_argument("--show_time", "-t", action="store_true", dest="show_time")
     args = app.parse_args()
     if args.k < 1:
@@ -37,8 +177,9 @@ def eprint(msg, end="\n"):
     sys.stderr.write(msg + end)
 
 
-def read_strings(input_file, v=False):
+def read_strings(input_file):
     """Read and check input stings."""
+    allowed_chars = {"1", "0"}
     with open(input_file, "r") as f:
         strings = [l.rstrip() for l in f.readlines()]
     # check data size
@@ -48,157 +189,26 @@ def read_strings(input_file, v=False):
     sys.exit("Error: string length must be >= 2!") if str_num < 2 else None
     if any(len(strings[i]) != str_len for i in range(str_num)):
         sys.exit("Error: strings must have the same lenght!")
-    eprint("Input data contains {} strings of length {}".format(str_num, str_len)) if v else None
-    all_characters = set(flatten(strings))
-    if all_characters.difference(CHARS):
+    all_characters = set(BDCspSolver.flatten(strings))
+    if all_characters.difference(allowed_chars):
         sys.exit("Error: Allowed characters are 1 and 0!")
     # ok, let's convert them into an arrray
     strings_int = [[int(c) for c in s] for s in strings]
-    return strings_int, str_num, str_len
-
-
-def flatten(lst):
-    """Flatten a list of lists into a list."""
-    return [item for sublist in lst for item in sublist]
-
-
-def invert_pattern(pattern):
-    """Return NOT pattern."""
-    return [0 if v == 1 else 1 for v in pattern]
-
-
-def extract_patterns(strings, str_num, str_len):
-    """Extract patterns from the strings."""
-    columns = [[s[n] for s in strings] for n in range(str_len)]
-    actual_col_num = str_len
-    id_to_pattern = []
-    pattern_id_to_positions = {}
-    pattern_to_positions = defaultdict(list)
-    for num, column in enumerate(columns):
-        if len(set(column)) == 1:
-            actual_col_num -= 1
-            continue
-        ones_pattern = tuple(column)
-        zeros_pattern = tuple(invert_pattern(ones_pattern))
-        # two patterns per position
-        pattern_to_positions[ones_pattern].append(num)
-        pattern_to_positions[zeros_pattern].append(num)
-    patterns_sorted = sorted([p for p in pattern_to_positions.keys()],
-                             key=lambda x: (sum(x), -1 * x.index(1)),
-                             reverse=True)
-    for num, pattern in enumerate(patterns_sorted):
-        id_to_pattern.append(pattern)
-        pattern_id_to_positions[num] = pattern_to_positions[pattern]
-    return id_to_pattern, pattern_id_to_positions, actual_col_num
-
-
-def reformate_pattern_id_to_pos(pat_id_to_pos_dct):
-    """Make a list instead of dict."""
-    max_key = max(pat_id_to_pos_dct.keys())
-    pat_id_to_pos = [[] for _ in range(max_key + 1)]
-    positions_to_patterns = defaultdict(list)
-    for pat_id in range(max_key + 1):
-        pos_ = pat_id_to_pos_dct[pat_id]
-        pat_id_to_pos[pat_id] = pos_
-        for p_ in pos_:
-            positions_to_patterns[p_].append(pat_id)
-    all_positions = sorted(positions_to_patterns.keys())
-    pos_to_pat_ids = [tuple() for _ in range(max(all_positions) + 1)]
-    for pos_ in all_positions:
-        pat_ids = tuple(sorted(positions_to_patterns[pos_]))
-        pos_to_pat_ids[pos_] = pat_ids
-    return pat_id_to_pos, pos_to_pat_ids, all_positions
+    return strings_int
 
 
 def main():
     """Enrty point."""
     # TODO: rewrite as a class
     t0 = dt.now()
-    # connect shared library
-    LIBS_DIR = os.path.join(os.getcwd(), "libs")
-    CSP_LIB_PATH = os.path.join(LIBS_DIR, "CSP.so")
-    if not os.path.isfile(CSP_LIB_PATH):
-        eprint("Error: shared libs not found\nCalling make...")
-        os.mkdir("libs") if not os.path.isdir("libs") else None
-        rc = subprocess.call("make", shell=True)
-        sys.exit("Make failed") if rc != 0 else eprint("Make successful")
-    CSP_SLIB = ctypes.CDLL(CSP_LIB_PATH)
-    # arguments:
-    CSP_SLIB.solve_CSP.argtypes = [ctypes.c_uint32,  # int 5 times
-                                   ctypes.c_uint32,
-                                   ctypes.c_uint32,
-                                   ctypes.c_uint32,
-                                   ctypes.c_uint32,  # int* 6 times
-                                   ctypes.POINTER(ctypes.c_uint8),
-                                   ctypes.POINTER(ctypes.c_uint32),
-                                   ctypes.POINTER(ctypes.c_uint32),
-                                   ctypes.POINTER(ctypes.c_uint32),
-                                   ctypes.POINTER(ctypes.c_uint32),
-                                   ctypes.POINTER(ctypes.c_uint32)]
-    # returns bool
-    CSP_SLIB.solve_CSP.restype = ctypes.c_bool
     # read and check input
     args = parse_args()
-    in_strings, str_num, str_len = read_strings(args.input_file, args.verbose)
-    # pre-process input
-    id_to_pattern, patttern_id_to_positions_raw, act_col_num \
-         = extract_patterns(in_strings, str_num, str_len)
-    sys.exit("Answer is:\nTrue") if act_col_num == 0 else None
-    sys.exit("Answer is:\nTrue") if (act_col_num - args.k) <= 0 else None
-    pattern_id_to_positions, positions_to_pat_ids, all_positions \
-        = reformate_pattern_id_to_pos(patttern_id_to_positions_raw)
-    # put this stuff into C function
-    patterns_num = len(id_to_pattern)
-    # pattern lenght is the str_num
-    patterns_array = flatten(id_to_pattern)
-    c_patterns_array = (ctypes.c_uint8 * (patterns_num * str_num + 1))()
-    c_patterns_array[:-1] = patterns_array
-    # patterns to positions
-    pat_to_pos_array = flatten(pattern_id_to_positions)
-    pat_to_pos_num = [len(x) for x in pattern_id_to_positions]
-    pat_to_pos_arr_size = len(pat_to_pos_array)
-    c_pat_to_pos_arr = (ctypes.c_uint32 * (pat_to_pos_arr_size + 1))()
-    c_pat_to_pos_arr[:-1] = pat_to_pos_array
-    c_pat_to_pos_num = (ctypes.c_uint32 * (patterns_num + 1))()
-    c_pat_to_pos_num[:-1] = pat_to_pos_num
-    pointer = 0
-    pat_to_pos_starts = []
-    for pos_ in pattern_id_to_positions:
-        pat_to_pos_starts.append(pointer)
-        pointer += len(pos_)
-    c_pat_to_pos_starts = (ctypes.c_uint32 * (patterns_num + 1))()
-    c_pat_to_pos_starts[:-1] = pat_to_pos_starts
-    # positions to patterns
-    pos_to_patterns_array = flatten(positions_to_pat_ids)
-    positions_num = len(all_positions)
-    c_pos_to_pat_array = (ctypes.c_uint32 * (positions_num * 2 + 1))()
-    c_pos_to_pat_array[:-1] = pos_to_patterns_array
-    # positions themselves
-    c_pos_array = (ctypes.c_uint32 * (positions_num + 1))()
-    c_pos_array[:-1] = all_positions
-    # single values
-    c_str_num = ctypes.c_uint32(str_num)
-    c_str_len = ctypes.c_uint32(act_col_num)
-    c_k_ = ctypes.c_uint32(args.k)
-    c_pos_num = ctypes.c_uint32(positions_num)
-    c_pat_num = ctypes.c_uint32(patterns_num)
-    # call the solver finally
-    answer = CSP_SLIB.solve_CSP(c_str_num,  # int
-                                c_str_len,  # int
-                                c_k_,  # int
-                                c_pat_num,  # int
-                                c_pos_num,  # int
-                                c_patterns_array,  # int*
-                                c_pat_to_pos_arr,  # int*
-                                c_pat_to_pos_num,  # int*
-                                c_pat_to_pos_starts,  # int*
-                                c_pos_array,
-                                c_pos_to_pat_array,  # int*
-    )
+    in_strings = read_strings(args.input_file)
+    solver = BDCspSolver(in_strings, args.k)
+    answer = solver.get_answer()
     print("# The answer is:\n{}".format(answer))
     eprint("Elapsed: {}".format(dt.now() - t0)) if args.show_time else None
     sys.exit("Done")
-    # Segmentation fault: 11!
 
 
 if __name__ == "__main__":

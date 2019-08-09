766b73ad2e1fb963668adf6ad20b09e9ff5884d8
Bogdan Kirilenko <kirilenkobm@gmail.com>
2019-08-10 00:57:23 +0200

    a little test added

---
+++ run_tests.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+echo "Running test 1, should return False"
+test1=$(./CSP input_data/test_0.txt 2> /dev/null 1)
+
+if [[ $? -ne 0 ]]
+then
+    echo "Test 1 returned != 0 exit code"
+    exit 1
+elif [[ $test1 = *False ]]
+then
+    echo "Test 1 - correct answer"
+else
+    echo "Test 1 - wrong answer"
+    exit 1
+fi
+
+echo "Running test 1, should return True"
+test2=$(./CSP input_data/test_0.txt 2> /dev/null 5)
+
+if [[ $? -ne 0 ]]
+then
+    echo "Test 2 returned != 0 exit code"
+    exit 1
+elif [[ $test2 = *True ]]
+then
+    echo "Test 2 - correct answer"
+else
+    echo "Test 2 - wrong answer"
+    exit 1
+fi

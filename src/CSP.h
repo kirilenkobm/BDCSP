3ba2d799a5d9bef8535d210d723ba4e37c1b78d7
Bogdan Kirilenko <kirilenkobm@gmail.com>
2019-08-17 19:26:24 +0200

    better free all

--- src/CSP.h
+++ src/CSP.h
@@ -16,7 +16,7 @@ void _show_usage_and_quit(char * executable);
 
 void verbose(const char * restrict format, ...);
 
-void free_all(uint32_t str_len, uint32_t str_num, uint32_t patterns_num);
+void free_all();
 
 uint32_t min_of_three(uint32_t *a, uint32_t *b, uint32_t *c);
 

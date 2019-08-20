f446f3b85052723dd82c38cc1a1ac6816ef6de9e
Bogdan Kirilenko <kirilenkobm@gmail.com>
2019-08-20 21:54:31 +0000

    not need this flags

--- Win_make.bat
+++ Win_make.bat
@@ -1,5 +1,5 @@
 :: like Makefile but for Windows
-SET "CL_PARAMS=/O2 /Iall /Iyour /Iincludes /D_USRDLL /DLL"
+SET "CL_PARAMS=/O2 /Iall /Iyour /Iincludes"
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat
 CALL cl %CL_PARAMS% src\CSP.c ^
                     src\arrstuff.c ^

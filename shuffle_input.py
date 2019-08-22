34b8be27f297fed6643a274013c89efea982be5d
Bogdan Kirilenko <kirilenkobm@gmail.com>
2019-08-22 12:16:59 +0200

    added random version of shuffle input

--- shuffle_input.py
+++ shuffle_input.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Shuffle input file defining line N as initial one."""
 import argparse
+import random
 import sys
 
 ALLOWED = {"1", "0", "\n", " "}
@@ -10,7 +11,8 @@ def parse_args():
     """Parse and check args."""
     app = argparse.ArgumentParser()
     app.add_argument("input_file", help="Input file to shuffle.")
-    app.add_argument("line", type=int, help="Number of line to make it initial one (0 - based)")
+    app.add_argument("line", type=str, help="Number of line to make it initial one (0 - based). "
+                                            "Write 'rnd' or 0 to randomize string order.")
     app.add_argument("--output", "-o", default="stdout", help="Output, default stdout.")
 
     if len(sys.argv) == 1:
@@ -37,11 +39,18 @@ def main():
     """Entry point."""
     args = parse_args()
     input_arr = read_input(args.input_file)
-    if args.line >= len(input_arr):
+    try:
+        l_num = 0 if args.line == "rnd" else int(args.line)
+    except ValueError:
+        sys.exit("Error! 'line' must be either a number or 'rnd' string")
+    if l_num >= len(input_arr):
         sys.exit("Error! Line {} (0-based) doesn't exist!".format(args.line))
-    temp = input_arr[0].copy()
-    input_arr[0] = input_arr[args.line].copy()
-    input_arr[args.line] = temp.copy()
+    if l_num != 0:
+        temp = input_arr[0].copy()
+        input_arr[0] = input_arr[args.line].copy()
+        input_arr[args.line] = temp.copy()
+    else:
+        random.shuffle(input_arr)
     f = open(args.output, "w") if args.output != "stdout" else sys.stdout
     for line in input_arr:
         out_line = "".join(str(x) for x in line)

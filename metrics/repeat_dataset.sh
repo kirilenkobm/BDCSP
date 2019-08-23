54354d85cb377f807f46b59abafe9eaf64de4b47
Bogdan Kirilenko <kirilenkobm@gmail.com>
2019-08-24 00:46:37 +0200

    fancier repeat dataset script

--- metrics/repeat_dataset.sh
+++ metrics/repeat_dataset.sh
@@ -2,14 +2,32 @@
 # repeat datasets for evaluation
 ./generate_input.py clean
 
-./generate_input.py 100 50 15 50 test_100_50_15
-./generate_input.py 100 50 25 50 test_100_50_25
-./generate_input.py 100 50 50 50 test_100_50_50
-./generate_input.py 100 50 75 50 test_100_50_75
+# tune this
+str_lens=(100 100 500 500)
+str_nums=(25 50 175 250)
+mult=(1 1 5 5)  # to change K accordingly
+range=4  # number of subsets
 
-./generate_input.py 1000 500 150 50 test_1000_500_150
-./generate_input.py 1000 500 250 50 test_1000_500_250
-./generate_input.py 1000 500 500 50 test_1000_500_500
-./generate_input.py 1000 500 750 50 test_1000_500_750
+# maybe this
+raw_ks=(15 25 50 75)
+raw_ks_max_i=3
+repl=20  # number of replicates
+
+for step in `seq 0 $((range - 1))`
+do
+    str_len="${str_lens[$step]}"
+    str_num="${str_nums[$step]}"
+    mul="${mult[$step]}"
+    echo "creating set of str_len $str_len and str_num $str_num"
+    for k_ in `seq 0 $raw_ks_max_i`
+    do
+        raw_k="${raw_ks[$k_]}"
+        this_k=$(($raw_k * mul))
+        d_name="test_${str_len}_${str_num}_${this_k}"
+        cmd="./generate_input.py $str_len $str_num $this_k $repl $d_name"
+        echo $cmd
+        eval $cmd
+    done
+done
 
 echo "Done."
